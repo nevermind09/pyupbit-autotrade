72dd7923266253efe6488257e13334d40367dcdc
nevermind09 <31715206+nevermind09@users.noreply.github.com>
2021-05-07 08:50:57 +0900

    Update bitcoinAutoTrade.py

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -21,9 +21,9 @@ def get_balance(ticker):
     """잔고 조회"""
     balances = upbit.get_balances()
     for b in balances:
-        if b['currency'] == ticker:
-            if b['balance'] is not None:
-                return float(b['balance'])
+        if b["currency"] == ticker:
+            if b["balance"] is not None:
+                return float(b["balance"])
             else:
                 return 0
 
