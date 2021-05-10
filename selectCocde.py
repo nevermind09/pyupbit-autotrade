ef90fb5270f47b7ce0d3e861312b47d214ce0895
nevermind09 <31715206+nevermind09@users.noreply.github.com>
2021-05-11 00:46:44 +0900

    Update selectCocde.py

--- selectCocde.py
+++ selectCocde.py
@@ -3,30 +3,28 @@ import pyupbit
 import datetime
 import pandas as pd
 
-allcode = pyupbit.get_tickers(fiat="KRW")
+def selectCode():
+    allcode = pyupbit.get_tickers(fiat="KRW")
 
-todaylist =[]*len(allcode)
+    todaylist =[]*len(allcode)
 
-for c in allcode:
-    i=0
-    print(c)
-    dfTemp = pyupbit.get_ohlcv(c, count=2)
-    t0amount = dfTemp.iloc[0][4] * (dfTemp.iloc[0][1] + dfTemp.iloc[0][2]) / 2
-    todaylist.append([c, t0amount])
-    print(todaylist)
-    print(dfTemp, "\n\n\n\n\n\n")
+    for c in allcode:
+        i=0
+        print(c)
+        dfTemp = pyupbit.get_ohlcv(c, count=2)
+        t0amount = dfTemp.iloc[0][4] * (dfTemp.iloc[0][1] + dfTemp.iloc[0][2]) / 2
+        todaylist.append([c, t0amount])
+        print(todaylist)
+        print(dfTemp, "\n\n\n\n\n\n")
 
-    time.sleep(0.3)
-    i = i+1
+        time.sleep(0.3)
+        i = i+1
 
 
-col_name = ['code', 'am']
-list_df = pd.DataFrame(todaylist, columns=col_name)
-list_df=list_df.sort_values(by=['am'], ascending=False)
-print(list_df.head(5))
+    col_name = ['code', 'am']
+    list_df = pd.DataFrame(todaylist, columns=col_name)
+    list_df=list_df.sort_values(by=['am'], ascending=False)
+    print(list_df.head(5))
 
-list_df.to_csv("today_list.csv")
+    list_df.to_csv("today_list.csv")
 
-print(dfTemp)
-
-print(dfTemp)
